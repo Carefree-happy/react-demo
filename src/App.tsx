87c1b99e06ee32002bb9f4856a7fbc47389dbd7c
sunjing <beiyouok@dtstack.com>
2024-08-22 13:37:16 +0800

    feat: effect 影响

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+
+async function queryData() {
+    const data = await new Promise<number>((resolve) => {
+        setTimeout(() => {
+            resolve(666);
+        }, 2000);
+    })
+    return data;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [num, setNum] = useState(0);
+
+    useEffect(() => {
+        queryData().then(data => {
+            setNum(data);
+        })
+    }, []);
+
+    return (
+        <div onClick={() => setNum((prevNum) => prevNum + 1)}>{num}</div>
+    );
 }
 
 export default App;
