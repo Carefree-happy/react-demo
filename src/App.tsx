64e2a570db5b6db852c8b4549caecf58ed4bbc7f
sunjing <beiyouok@dtstack.com>
2024-08-22 19:23:37 +0800

    feat: 用户更改状态

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import { ChangeEvent, useEffect, useRef } from 'react';
+import { ChangeEvent, useState } from 'react';
 
 const App = () => {
 
-    // 1.受控模式 value 由用户控制就是非受控模式，由代码控制就是受控模式
-    // function onChange(event: ChangeEvent<HTMLInputElement>) {
-    //     console.log(event.target.value)
-    // }
+    const [value, setValue] = useState('Sun')
 
-    // return <div>
-    //     <input defaultValue={'sun'} onChange={onChange}></input>
-    // </div>;
+    function onChange(event: ChangeEvent<HTMLInputElement>) {
+        console.log(event.target.value)
+        setValue(event.target.value)
+    }
 
-    // 2.ref
-    const inputRef = useRef<HTMLInputElement>(null)
-
-    useEffect(() => {
-        setTimeout(() => {
-            console.log(inputRef.current?.value)
-        }, 2000);
-    }, [])
-
-    return <input defaultValue={'Sun'} ref={inputRef}/>
+    return <input value={value} onChange={onChange}/>
 }
 
 export default App;
