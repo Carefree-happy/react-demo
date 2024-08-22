3844b63407b05d18b993b30447b6b5f48a4437b3
sunjing <beiyouok@dtstack.com>
2024-08-22 14:42:47 +0800

    feat: 定时器

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,41 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef, useCallback } from 'react';
 
-function App() {
+function useInterval(fn: Function, time: number) {
+    const ref = useRef(fn);
 
-    const [count,setCount] = useState(0);
+    ref.current = fn;
+
+    let cleanUpFnRef = useRef<Function>();
+    
+    const clean = useCallback(() =>{
+        cleanUpFnRef.current?.();
+    }, []);
 
     useEffect(() => {
-        console.log(count);
+        // 定时器的场景需要保证定时器只跑一次，不然重新跑会导致定时不准，所以需要用 useEffect + useRef 的方式来解决闭包陷阱问题
+        const timer = setInterval(() => ref.current(), time);
 
-        const timer = setInterval(() => {
-            setCount(count + 1);
-        }, 1000);
-
-        return () => {
+        cleanUpFnRef.current = ()=> {
             clearInterval(timer);
         }
-    }, [count]);
 
-    return <div>{count}</div>
+        return clean;
+    }, []);
+
+    return clean;
+}
+
+
+function App() {
+    const [count, setCount] = useState(0);
+
+    const updateCount = () => {
+        setCount(count + 1);
+    };
+
+    useInterval(updateCount, 1000);
+
+    return <div>{count}</div>;
 }
 
 export default App;
