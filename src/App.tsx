45cb48274f095bc6b4260e4e3cd2d8a20e80ae4f
sunjing <beiyouok@dtstack.com>
2024-08-23 17:08:51 +0800

    test: 查找文本内容断言文本在document内

--- src/App.tsx
+++ src/App.tsx
@@ -1,132 +1,26 @@
-import React, { useEffect, useImperativeHandle, useRef, useState } from 'react';
-import './index.css';
-import { useControllableValue } from 'ahooks';
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-interface CalendarProps {
-  value?: Date,
-  defaultValue?: Date,
-  onChange?: (date: Date) => void
-}
-
-interface CalendarRef {
-  getDate: () => Date,
-  setDate: (date: Date) => void,
-}
-
-const InternalCalendar: React.ForwardRefRenderFunction<CalendarRef, CalendarProps> = (props, ref) => {
-  const {
-    value,
-    defaultValue,
-    onChange,
-  } = props;
-
-  const [date, setDate] =  useControllableValue(props,{
-    defaultValue: new Date()
-  });
-
-  useImperativeHandle(ref, () => {
-    return {
-      getDate() {
-        return date;
-      },
-      setDate(date: Date) {
-        setDate(date)
-      }
-    }
-  });
-
-  const handlePrevMonth = () => {
-    setDate(new Date(date.getFullYear(), date.getMonth() - 1, 1));
-  };
-
-  const handleNextMonth = () => {
-    setDate(new Date(date.getFullYear(), date.getMonth() + 1, 1));
-  };
-
-  const monthNames = [
-    '一月',
-    '二月',
-    '三月',
-    '四月',
-    '五月',
-    '六月',
-    '七月',
-    '八月',
-    '九月',
-    '十月',
-    '十一月',
-    '十二月',
-  ];
-
-  const daysOfMonth = (year: number, month: number) => {
-    return new Date(year, month + 1, 0).getDate();
-  };
-
-  const firstDayOfMonth = (year: number, month: number) => {
-    return new Date(year, month, 1).getDay();
-  };
-
-  const renderDates = () => {
-    const days = [];
-
-    const daysCount = daysOfMonth(date.getFullYear(), date.getMonth());
-    const firstDay = firstDayOfMonth(date.getFullYear(), date.getMonth());
-
-    for (let i = 0; i < firstDay; i++) {
-      days.push(<div key={`empty-${i}`} className="empty"></div>);
-    }
-
-    for (let i = 1; i <= daysCount; i++) {
-      const clickHandler = () => {
-        const curDate = new Date(date.getFullYear(), date.getMonth(), i);
-        setDate(curDate);
-      }
-    
-      if(i === date.getDate()) {
-        days.push(<div key={i} className="day selected" onClick={() => clickHandler()}>{i}</div>);  
-      } else {
-        days.push(<div key={i} className="day" onClick={() => clickHandler()}>{i}</div>);
-      }
-    }
-
-    return days;
-  };
-
+function App() {
   return (
-    <div className="calendar">
-      <div className="header">
-        <button onClick={handlePrevMonth}>&lt;</button>
-        <div>{date.getFullYear()}年{monthNames[date.getMonth()]}</div>
-        <button onClick={handleNextMonth}>&gt;</button>
-      </div>
-      <div className="days">
-        <div className="day">日</div>
-        <div className="day">一</div>
-        <div className="day">二</div>
-        <div className="day">三</div>
-        <div className="day">四</div>
-        <div className="day">五</div>
-        <div className="day">六</div>
-        {renderDates()}
-      </div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
 
-const Calendar = React.forwardRef(InternalCalendar);
-
-function Test() {
-  const [date, setDate] = useState(new Date());
-
-  return <Calendar value={date} onChange={(newDate) => {
-      setDate(newDate);
-      alert(newDate.toLocaleDateString());
-  }}></Calendar>
-
-  // return <Calendar defaultValue={new Date()} onChange={(newDate) => {
-  //     alert(newDate.toLocaleDateString());
-  // }}></Calendar>
-
-}
-
-export default Test;
+export default App;
