74013767c56e2569e9af2d61eedcd5c522189ddf
sunjing <beiyouok@dtstack.com>
2024-08-23 17:39:30 +0800

    test: timeout

--- src/components/Toggle/Toggle.test.tsx
+++ src/components/Toggle/Toggle.test.tsx
@@ -8,12 +8,18 @@ import Toggle from './Toggle';
 //   expect(container.querySelector('p')?.textContent).toBe('open');
 // });
 
-test('toggle', () => {
+test('toggle', async() => {
   render(<Toggle />);
   // 检查初始状态下 <p> 标签的文本是否为 'close'
   expect(screen.getByText('close')).toBeInTheDocument();
   // 点击按钮
   fireEvent.click(screen.getByRole('button'));
-  // 检查点击后 <p> 标签的文本是否为 'open'
-  expect(screen.getByText('open')).toBeInTheDocument();
+  // 1.检查点击后 <p> 标签的文本是否为 'open'
+  // expect(screen.getByText('open')).toBeInTheDocument();
+  // 2.等待文本内容从 'close' 变为 'open'
+  // await waitFor(() => expect(screen.getByText('open')).toBeInTheDocument(), {
+  //   timeout: 3000,
+  // });
+  const openText = await screen.findByText('open', {}, { timeout: 3000 });
+  expect(openText).toBeInTheDocument();
 });
