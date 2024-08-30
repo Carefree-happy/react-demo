9597840420f37b11cbb1a0430371b0b11da1338d
sunjing <beiyouok@dtstack.com>
2024-08-30 15:14:10 +0800

    feat: Space

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,9 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 import './App.css';
-import Toggle from './components/Toggle/Toggle';
-import useCounter from './hooks/Counter/useCounter';
-import { IconAdd } from './icon/IconAdd';
-import { IconEmail } from './icon/IconEmail';
-import { createFromIconfont } from './icon/createFrontIconfont';
-
-let data: string, promise;
-
-function fetchData() {
-  if (data) return data;
-  promise = new Promise<void>(resolve => {
-    setTimeout(() => {
-      data = '取到的数据'
-      resolve()
-    }, 2000)
-  })
-  throw promise;
-}
-
-function Content() {
-  const data = fetchData();
-  return <p>{data}</p>
-}
-
-const IconFont = createFromIconfont('//at.alicdn.com/t/c/font_4443338_a2wwqhorbk4.js');
+import Space from './components/Space';
+import { ConfigProvider } from './components/Space/ConfigProvider';
 
 function App() {
-  const [count, increment, decrement] = useCounter(100);
   return (
     <div className="App">
       <header className='App-header'>
@@ -39,20 +15,18 @@ function App() {
         >
           Learn React
         </a>
-        <Toggle />
-        <button onClick={() => increment(1)}>+</button>
-        <button onClick={() => decrement(1)}>1</button>
-        <p>{count}</p>
-        <Suspense fallback={'loading data'}>
-          <Content />
-        </Suspense>
-        <div style={ {padding: '50px'} }>
-          <IconAdd size='40px'></IconAdd>
-          <IconEmail spin></IconEmail>
-          <IconEmail style={{color: 'blue', fontSize: '50px'}}></IconEmail>
-          <IconFont type="icon-shouye-zhihui" size="40px"></IconFont>
-          <IconFont type="icon-gerenzhongxin-zhihui" fill='red' size="90px"></IconFont>
-        </div>
+        <ConfigProvider space={{ size: 20 }}>
+          <Space direction="horizontal">
+            <div className="box">1</div>
+            <div className="box">2</div>
+            <div className="box">3</div>
+          </Space>
+          <Space direction="vertical">
+            <div className="box">4</div>
+            <div className="box">5</div>
+            <div className="box">6</div>
+          </Space>
+        </ConfigProvider>
       </header>
       
     </div>
